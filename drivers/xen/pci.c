2ecce5d218c0a8879a15fc78a5f5d76f5591ee7c
Leonid Komarianskyi <leonid_komarianskyi@epam.com>
2026-07-28 14:52:16 +0300

    Revert "xen/pci: Always notify xen of new PCI devices"
    This reverts commit 2be7bb907647ac65a5b7d12f6701cbd738304f8d.

--- drivers/xen/pci.c
+++ drivers/xen/pci.c
@@ -247,6 +247,9 @@ static struct notifier_block device_nb = {
 
 static int __init register_xen_pci_notifier(void)
 {
+	if (!xen_initial_domain())
+		return 0;
+
 	return bus_register_notifier(&pci_bus_type, &device_nb);
 }
 
